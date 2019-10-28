7351045646aace3ad9e82df0234b90e3e1e5ebc3
Zhangjunyao <1343671380@qq.com>
2019-10-28 16:19:07 +0800

    把本地工程添加到git

---
+++ gittest/src/com/stx/FirstClass.java
@@ -0,0 +1,11 @@
+package com.stx;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("Hello git project");
+        System.out.println("Hello git project");
+        System.out.println("Hello git project");
+        System.out.println("Hello git project");
+        System.out.println("Hello git project");
+    }
+}
